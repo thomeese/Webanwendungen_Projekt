ea123f83648e81e2c0f5e88504ba6cc75618b6c4
Manuel Arling <manuel.arling@hs-osnabrueck.de>
2022-09-01 15:33:36 +0200

    push fire

---
+++ webapp/train-smarter/src/app/services/authentication.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import firebase from 'firebase/compat';
+import {Auth, signInWithEmailAndPassword, signOut} from '@angular/fire/auth';
+import {createUserWithEmailAndPassword} from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor(private auth: Auth) {
+  }
+
+  async signUp({email, password}) {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        this.auth,
+        email,
+        password
+      );
+      return user;
+    } catch (ex) {
+      return null;
+    }
+  }
+
+  async signIn({email, password}) {
+    try {
+      const user = await signInWithEmailAndPassword(
+        this.auth,
+        email,
+        password
+      );
+      return user;
+    } catch (ex) {
+      return null;
+    }
+  }
+
+  signOut(): Promise<void> {
+    return signOut(this.auth);
+  }
+}
