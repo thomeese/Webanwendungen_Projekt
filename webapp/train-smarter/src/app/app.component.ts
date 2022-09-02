9147007aa80e8e20930c9b58a3394def6bfd93a9
Thomas Meese <thomas.meese@hs-osnabrueck.de>
2022-09-02 14:36:26 +0200

    Login Lüppt

--- webapp/train-smarter/src/app/app.component.ts
+++ webapp/train-smarter/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {Router} from '@angular/router';
+import {AuthenticationService} from './services/authentication.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -7,11 +9,22 @@ import {Router} from '@angular/router';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'home', url: 'home', icon: 'paper-plane' },
-    { title: 'live-training', url: 'live-training', icon: 'heart' },
-    { title: 'analytics', url: 'analytics', icon: 'archive' },
-    { title: 'training-plan', url: 'training-plan', icon: 'trash' },
-    { title: 'exercise', url: 'exercise', icon: 'warning' },
+    {title: 'home', url: 'home', icon: 'paper-plane'},
+    {title: 'live-training', url: 'live-training', icon: 'heart'},
+    {title: 'analytics', url: 'analytics', icon: 'archive'},
+    {title: 'training-plan', url: 'training-plan', icon: 'trash'},
+    {title: 'exercise', url: 'exercise', icon: 'warning'},
   ];
-  constructor(private router: Router) {}
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {
+  }
+
+  async signOut() {
+    await this.authService.signOut();
+    this.router.navigateByUrl('/login',{replaceUrl: true});
+    document.location.reload();
+  }
 }
