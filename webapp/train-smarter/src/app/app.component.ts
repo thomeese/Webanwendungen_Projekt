2568e5800638ee7ab2887f40fdf3f80a42880cf6
Thomas Meese <thomas.meese@hs-osnabrueck.de>
2022-09-02 16:14:23 +0200

    Login Lüppt

--- webapp/train-smarter/src/app/app.component.ts
+++ webapp/train-smarter/src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
     {title: 'home', url: 'home', icon: 'paper-plane'},
     {title: 'live-training', url: 'live-training', icon: 'heart'},
     {title: 'analytics', url: 'analytics', icon: 'archive'},
-    {title: 'training-plan', url: 'training-plan', icon: 'trash'},
+    {title: 'training-plan', url: 'training-plan', icon: 'document'},
     {title: 'exercise', url: 'exercise', icon: 'warning'},
   ];
 
