5f8b17c6339cf4701853d7a91cc4508018a2c3ce
Manuel Arling <manuel.arling@hs-osnabrueck.de>
2022-09-05 14:46:57 +0200

    Aktueller stand mit Animationsbug

--- webapp/train-smarter/src/app/pages/exercise/exercise-detail/exercise-detail.page.ts
+++ webapp/train-smarter/src/app/pages/exercise/exercise-detail/exercise-detail.page.ts
@@ -1,9 +1,22 @@
 import {Component, OnInit} from '@angular/core';
 import {ExerciseDBService} from '../../../services/exercise-db.service';
 import {ActivatedRoute} from '@angular/router';
+import { trigger, transition, animate, style } from '@angular/animations';
+import {LoadingController} from "@ionic/angular";
 
 @Component({
   selector: 'app-exercise-detail',
+  animations: [
+    trigger('myInsertRemoveTrigger', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('100ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('100ms', style({ opacity: 0 }))
+      ])
+    ]),
+  ],
   templateUrl: './exercise-detail.page.html',
   styleUrls: ['./exercise-detail.page.scss'],
 })
@@ -11,18 +24,28 @@ export class ExerciseDetailPage implements OnInit {
 
   id;
   exercise;
-  constructor(private exerciseDBService: ExerciseDBService, private route: ActivatedRoute) {
+  constructor(private exerciseDBService: ExerciseDBService, private route: ActivatedRoute,
+              private loadingController: LoadingController) {
   }
 
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
-
+    this.getExercise();
   }
 
-  getExercise(){
-    this.exerciseDBService.getExerciseByID(this.id).subscribe((result) => {
+  async getExercise(){
+    const loading = await this.loadingController.create({
+      spinner: null,
+      duration: 5000,
+      message: 'Please wait...',
+      translucent: true,
+      cssClass: 'custom-class custom-loading'
+    });
+    await loading.present();
+    await this.exerciseDBService.getExerciseByID(this.id).subscribe((result) => {
       this.exercise = result;
       console.log(this.exercise);
     });
+    await loading.dismiss();
   }
 }
