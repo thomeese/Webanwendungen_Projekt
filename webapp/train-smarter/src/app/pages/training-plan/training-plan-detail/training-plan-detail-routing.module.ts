def930a495d1f60213f85f920567c0de4fb5573e
Thomas Meese <thomas.meese@hs-osnabrueck.de>
2022-09-05 15:28:29 +0200

    Stand

--- webapp/train-smarter/src/app/pages/training-plan/training-plan-detail/training-plan-detail-routing.module.ts
+++ webapp/train-smarter/src/app/pages/training-plan/training-plan-detail/training-plan-detail-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TrainingPlanDetailPage } from './training-plan-detail.page';
+import {ExerciseLoggingPage} from '../../exercise-logging/exercise-logging.page';
 
 const routes: Routes = [
   {
     path: '',
     component: TrainingPlanDetailPage
+  },
+  {
+    path: ':id',
+    component: ExerciseLoggingPage
   }
 ];
 
