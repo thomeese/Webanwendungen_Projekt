dd9db827a361a61582072212d359aa644001ee77
Manuel Arling <manuel.arling@hs-osnabrueck.de>
2022-09-08 17:23:56 +0200

    AllreadyLogin hinzugefeugt

--- webapp/train-smarter/src/app/app.module.ts
+++ webapp/train-smarter/src/app/app.module.ts
@@ -1,10 +1,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouteReuseStrategy} from '@angular/router';
-
 import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
-
-
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
@@ -16,6 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
 import {pageAnimation} from './animations/nav-animation';
 import {Capacitor} from '@capacitor/core';
 import {indexedDBLocalPersistence} from '@firebase/auth';
+import {Geolocation} from '@ionic-native/geolocation/ngx';
 
 @NgModule({
   declarations: [AppComponent],
