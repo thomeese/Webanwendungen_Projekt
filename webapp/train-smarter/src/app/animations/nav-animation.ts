4a25872b79d15eb35fab7a45344dc99bd74f3479
Manuel Arling <manuel.arling@hs-osnabrueck.de>
2022-09-10 17:27:04 +0200

    Design

--- webapp/train-smarter/src/app/animations/nav-animation.ts
+++ webapp/train-smarter/src/app/animations/nav-animation.ts
@@ -1,33 +1,32 @@
 import {AnimationController, Animation} from '@ionic/angular';
 
-
 export const pageAnimation = (baseEL: HTMLElement, opts?: any): Animation => {
   const DURATION = 300;
 
   const animationCtrl = new AnimationController();
-  // root animation with common setup for the whole transition
+  // Standard Transition
   const rootTransition = animationCtrl.create()
     .duration(opts.duration || DURATION)
     .easing('cubic-bezier(0.3,0,0.66,1)');
 
-  // ensure that the entering page is visible from the start of the transition
+  // entfernt ion-page-invisible, damit die neue Page direkt sichtbar ist
   const enteringPage = animationCtrl.create()
     .addElement(opts.enteringEl)
     .beforeRemoveClass('ion-page-invisible');
 
-  // create animation for the leaving page
+  // erstellt eine Animation für die verlassende Seite
   const leavingPage = animationCtrl.create().addElement(opts.leavingEl);
 
-  // actual customized animation
+  // eigentliche Animation für die das Eintreten und Verlassen der jeweiligen Page
   if (opts.direction === 'forward') {
-    enteringPage.fromTo('opacity', '0.1', '1').fromTo('transform', 'translateY(100%)', 'translateY(0)');
+    enteringPage.fromTo('opacity', '0.1', '1').fromTo('transform', 'translateX(100%)', 'translateX(0)');
     leavingPage.fromTo('opacity', '1', '0.1');
   } else {
-    leavingPage.fromTo('transform', 'translateY(0)', 'translateY(100%)');
+    leavingPage.fromTo('transform', 'translateX(0)', 'translateX(100%)');
     enteringPage.fromTo('opacity', '0.1', '1');
   }
 
-  // include animations for both pages into the root animation
+  // Fuegt Animation für das Eintreten und Verlassen der Seiten hinzu
   rootTransition.addAnimation(enteringPage);
   rootTransition.addAnimation(leavingPage);
   return rootTransition;
