d163d0fccbe5b444357a964505c4dc62f8084515
Manuel Arling <manuel.arling@hs-osnabrueck.de>
2022-09-12 02:17:37 +0200

    Calendar teilweise eingebunden

---
+++ webapp/train-smarter/src/app/services/calendar.service.ts
@@ -0,0 +1,46 @@
+import {Injectable} from '@angular/core';
+import {Calendar} from '@awesome-cordova-plugins/calendar/ngx';
+import {Platform} from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalendarService {
+
+  calendarId;
+  trainSmarterCalendar;
+
+  constructor(private calendar: Calendar,
+              private platform: Platform) {
+    this.platform.ready().then(() => {
+      if (this.platform.is('ios')) {
+        this.calendar.createCalendar({calendarName: 'Train-Smarter', calendarColor: '#1e88fc'}).then((r) => {
+          this.getCalendar();
+        });
+      } else { // Android
+          this.getCalendar();
+      }
+    });
+  }
+
+  getCalendar() {
+    this.calendar.listCalendars().then(calendars => {
+      for (const cal of calendars) {
+        if (cal.name === 'Train-Smarter') {
+          this.trainSmarterCalendar = cal;
+          console.log(this.trainSmarterCalendar);
+        }
+      }
+    });
+  }
+
+  addToCalendar(data) {
+    const date = new Date();
+    console.log(date);
+    console.log(data);
+    const options = {calendarId: this.trainSmarterCalendar.id, calendarName: this.trainSmarterCalendar.name, firstReminderMinutes: 30};
+    this.calendar.createEventInteractivelyWithOptions(data.name, null, data.description, date, date, options).then(() => {
+
+    });
+  }
+}
